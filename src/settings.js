2c10a86975a1722addcfce4c8ec214333eda6217
re-fort <jeffpko@gmail.com>
2017-03-07 00:13:11 +0900

    :lock: Use HTTPS

--- src/settings.js
+++ src/settings.js
@@ -4,6 +4,6 @@ export default {
     // root: 'http://localhost:3000/api/'
 
     // production
-    root: 'http://ngzk-blog-archive-restapi.herokuapp.com/api/'
+    root: 'https://ngzk-blog-archive-restapi.herokuapp.com/api/'
   }
 }
