76ef885917dcee53f7bf38026613ccb801e42485
Saori <Saori@users.noreply.github.com>
2020-11-26 05:57:45 +0000

    :green_heart: CI build

---
+++ precache-manifest.4b4931bd01924e3fe94fc067b4ae0ccb.js
@@ -0,0 +1,83 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1c0d690b5c08414131e73faf48ee08",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "38c0d0cbc5bec2cc882843e24ba0c1dc",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "43051252d1635cc655970cadae18c8b9",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "a930a31b5825976a1d086283ec168c3a",
+    "url": "index.html"
+  },
+  {
+    "url": "js/app.0601aba0598f923c9107.js"
+  },
+  {
+    "url": "js/vendor.0601aba0598f923c9107.js"
+  },
+  {
+    "revision": "c90ebf13c749d81f72dd13cd919e3033",
+    "url": "manifest.json"
+  },
+  {
+    "url": "static/fontawesome-webfont.674f50d287a8c48dc19ba404d20fe713.eot"
+  },
+  {
+    "url": "static/fontawesome-webfont.912ec66d7572ff821749319396470bde.svg"
+  },
+  {
+    "url": "static/fontawesome-webfont.af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "static/fontawesome-webfont.b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "static/fontawesome-webfont.fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "revision": "07fd0ba602ffacbcbdd7ba548e5b7373",
+    "url": "static/icons/icon-128x128.png"
+  },
+  {
+    "revision": "afe62508e332c659c0c027f1da9c8458",
+    "url": "static/icons/icon-144x144.png"
+  },
+  {
+    "revision": "f9302a115641f6efd5c64ac1c2f6bb4c",
+    "url": "static/icons/icon-152x152.png"
+  },
+  {
+    "revision": "29ed13977e2676a4760088aa272b5f61",
+    "url": "static/icons/icon-192x192.png"
+  },
+  {
+    "revision": "fabef66244d8d77e6fa02db32ff896fc",
+    "url": "static/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c2dbda02983e1b76eab7f01a14a21c98",
+    "url": "static/icons/icon-512x512.png"
+  },
+  {
+    "revision": "8de3cff77a671adfe9d6727870a2a44e",
+    "url": "static/icons/icon-72x72.png"
+  },
+  {
+    "revision": "00fd2ba8e904ab7a4f33ae760e173138",
+    "url": "static/icons/icon-96x96.png"
+  },
+  {
+    "revision": "c50d79792bb3a5b50a8f",
+    "url": "stylesheets/app.8acb739c98475c118fee7a7db3c74e0b.css"
+  },
+  {
+    "revision": "7b7f531ff08a234f3faf",
+    "url": "stylesheets/vendor.c186a0bd7962791e9552654277f15af7.css"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.4b4931bd01924e3fe94fc067b4ae0ccb.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/https:\/\/ngzk-blog-archieve-resources.netlify.com\//, new workbox.strategies.CacheFirst({ "cacheName":"api-cache", plugins: [new workbox.expiration.Plugin({ maxEntries: 100, maxAgeSeconds: 1209600, purgeOnQuotaError: false }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
